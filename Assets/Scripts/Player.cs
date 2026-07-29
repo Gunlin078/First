d5a6fe8822ec9c3126db640f37530f2836de78cc
Gunlin <r.a.ft@mail.ru>
2026-07-29 17:41:44 +0200

    Анимации при всех движениях и спокойный фон

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,16 +3,19 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float moveSpeed = 5f;
-    public float jumpForse = 6f;
+    public float jumpForse = 10f;
     public Transform groundCheck;
     public float groundCheckRadius = 0.2f;
     public LayerMask groundLayer;
     
     private Rigidbody2D rb;
     private bool isGrounded;
+
+    private Animator animator;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     void Update()
@@ -24,10 +27,22 @@ public class Player : MonoBehaviour
         {
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForse);
         }
+        SetAnimation(moveInput);
     }
 
     private void FixedUpdate()
     {
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+    }
+
+    private void SetAnimation(float moveInput){
+        if (isGrounded) {
+            if (moveInput == 0) {animator.Play("Player_Idle"); }  
+            else                {animator.Play("Player_Run");  }
+        }
+        else {
+            if (rb.linearVelocityY > 0) {animator.Play("Player_Jump"); }
+            else                        {animator.Play("Player_Fall"); }
+        }
     }
 }
