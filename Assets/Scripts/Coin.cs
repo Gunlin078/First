5591941e5a263c9142f77d9b926837e2096fdfd4
Gunlin <r.a.ft@mail.ru>
2026-08-02 00:21:21 +0200

    Добавил монеты разной ценности

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -1,12 +1,20 @@
 using UnityEngine;
+using TMPro;
 
 public class Coin : MonoBehaviour
 {
+    public int coinsToGive;
+    private TextMeshProUGUI coinText;
+    private void Start()
+    {
+        coinText = GameObject.FindWithTag("CoinText").GetComponent<TextMeshProUGUI>();
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player")) {
             Player player = collision.gameObject.GetComponent<Player>();
-            player.coins += 1;
+            player.coins += coinsToGive;
+            coinText.text = player.coins.ToString();
             Destroy(gameObject);
         }
     }
