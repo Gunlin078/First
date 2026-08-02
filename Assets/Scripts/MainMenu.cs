09980f7c0aba78ff21d8339cd55ea7006572498e
Gunlin <r.a.ft@mail.ru>
2026-08-02 19:52:24 +0200

    Добавлен выбор уровня в начале

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,8 +1,24 @@
 using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
 using UnityEngine;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject mainMenu;
+    public GameObject levelSelect;
+    public void LoadLevel(string levelName){
+        SceneManager.LoadScene(levelName);
+    }
+    void Update() {
+        if (Keyboard.current != null && Keyboard.current.escapeKey.wasPressedThisFrame){
+            mainMenu.SetActive(true);
+            levelSelect.SetActive(false);
+        }
+    }
+    public void OpenLevelSelect() { 
+        mainMenu.SetActive(false);
+        levelSelect.SetActive(true);
+    }
     public void StartGame(){
         SceneManager.LoadScene("GameScene");
     }
